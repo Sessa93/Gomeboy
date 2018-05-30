d5f6da1a084ded2f82205c245824c51a39a5aea4
Andrea Sessa <andrea.sessa@cleafy.com>
2018-05-30 16:46:53 +0200

    Added CPU flags register

---
+++ src/cpu/flags.go
@@ -0,0 +1,62 @@
+package cpu
+
+import (
+	"fmt"
+)
+
+type Flags struct {
+	Z bool // Zero bit
+	N bool
+	H bool 
+	C bool // Carry bit
+}
+
+func (Flags f) isZ() bool {
+	return f.Z
+}
+
+func (Flags f) isN() bool {
+	return f.N
+}
+
+func (Flags f) isH() bool {
+	return f.H
+}
+
+func (Flags f) isC() bool {
+	return f.C
+}
+
+
+func (Flags f) setZ() {
+	f.Z := true
+}
+
+func (Flags f) setN() {
+	f.N := true
+}
+
+func (Flags f) setH() {
+	f.H := true
+}
+
+func (Flags f) setC() {
+	f.C := true
+}
+
+
+func (Flags f) clearZ() {
+	f.Z := false
+}
+
+func (Flags f) clearN() {
+	f.N := false
+}
+
+func (Flags f) clearH() {
+	f.H := false
+}
+
+func (Flags f) clearC() {
+	f.C := false
+}
